f5a3bf20ace3880399b261a70253e4cc1e914f73
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-07-17 11:23:41 +0200

    chore(): change version file: 0.3.0-beta.0

--- test/cart.test.js
+++ test/cart.test.js
@@ -5,6 +5,7 @@ const { CartDAO } = require('./../cart');
 const MongoClient = require('mongodb').MongoClient;
 
 /* global define, it, describe, before, beforeEach, afterEach, after */
+const USER_ID = '558098a65133816958968d88';
 describe('Test Carts', () => {
   let db = null;
   let cart = null;
@@ -18,13 +19,30 @@ describe('Test Carts', () => {
   const getCartPromisified = ({ userId }) => new Promise((resolve) =>
     cart.getCart(userId, resolve));
   it('Should get cart', () => {
-    return getCartPromisified({userId: '558098a65133816958968d88'})
+    return getCartPromisified({ userId: USER_ID })
       .then(cartUser => {
         expect(cartUser.items.length).to.be.equal(5);
         const totalCostCart = cartUser.items.reduce((prev, item) =>
           prev + item.quantity * item.price, 0);
         expect(totalCostCart).to.be.equal(399.97);
       });
+  });
+  const getItemInCartPromisified = ({ userId, itemId }) => new Promise(resolve =>
+    cart.itemInCart(userId, itemId, resolve));
+  it('Should get null for an item that is not in the cart', () => {
+    return getItemInCartPromisified({
+      userId: USER_ID,
+      itemId: 3
+    }).then(item => expect(item).to.be.null);
+  });
+  it('Should get an item that is in the cart', () => {
+    return getItemInCartPromisified({
+      userId: USER_ID,
+      itemId: 2
+    }).then(item => {
+      expect(item._id).to.be.equal(2);
+      expect(item.quantity).to.be.equal(12);
+    });
   });
   after('Close connection', () => {
     db.close();
